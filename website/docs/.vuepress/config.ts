5c00bacf52a422ac56e2851b795bbdd74a612fe4
S Turple <shawn.turple@gmail.com>
2023-11-05 16:14:26 -0800

    Adding code for first article

--- website/docs/.vuepress/config.ts
+++ website/docs/.vuepress/config.ts
@@ -1,16 +1,23 @@
 import { defaultTheme } from "@vuepress/theme-default";
+import { debug } from "@vuepress/utils";
 
 import { defineUserConfig } from "vuepress";
 import { searchPlugin } from "@vuepress/plugin-search";
+import { getDirname, path } from '@vuepress/utils';
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+
+
+const __dirname = getDirname(import.meta.url)
+
 
 export default defineUserConfig({
   base: "/",
   lang: "en-US",
-  title: "Shawn Turple",
+  title: "Turple.ca",
   description: "Turple.ca",
   theme: defaultTheme({
     editLink: false,
-    lastUpdated: false,
+    lastUpdated: true,
     repo: 'sturple/sturple.github.io',
     repoLabel: 'Github',
     navbar: [
@@ -18,19 +25,22 @@ export default defineUserConfig({
         text: 'Home',
         link: '/',
       },
-      {
-        text: 'Containers',
-        link: '/guide/containers/index.md',
-      },
-      {
-        text: 'Kubernetes',
-        link: '/guide/kubernetes/index.md',
-      },
+      // {
+      //   text: 'Containers',
+      //   link: '/guide/containers/index.md',
+      // },
+      // {
+      //   text: 'Kubernetes',
+      //   link: '/guide/kubernetes/index.md',
+      // },
     ],
   }),
   plugins: [
     searchPlugin({
       // options
-    })
+    }),
+    googleAnalyticsPlugin({
+      id: 'G-FX6HXTQK7G'
+    }),
   ],
 });
